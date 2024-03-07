b2ac48643edf3ea347e24fd64456fb8ec2ccfca9
Andrea Guerrieri <aguerrieri@eusoft.net>
2024-03-07 18:18:57 +0100

    magixphysx android

--- src/libmagicphysx/android-build.cmd
+++ src/libmagicphysx/android-build.cmd
@@ -1,6 +1,10 @@
 rustup target add aarch64-linux-android
-install cargo-ndk
+
+REM install cargo-ndk
  
 SET ANDROID_NDK_ROOT=C:\Android\ndk\26.1.10909125\
+SET NDK_PROJECT_PATH=%CD%
 
 cargo ndk --platform 26 --target aarch64-linux-android build --release 
+
+REM call %ANDROID_NDK_ROOT%\build\ndk-build
